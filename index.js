4d80527961795f78575b7ff4b9efdf4dfc91767b
ebignumber <ebignumber@proton.me>
2026-04-17 22:48:28 -0400

    Save board state to localStorage

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 let scores = [100, 200, 300, 400, 500, 600]
 let catagories = ["Topic 1", "Topic 2", "Topic 3", "Topic 4", "Topic 5"]
-
-function changeColor(element){
+let spacesFilled = JSON.parse(localStorage.getItem("Spaces Filled")) || []
+let test = []
+function clearSpace(element){
   if (element.className == "not-filled"){
     element.className = "filled"
+    spacesFilled.push(element.id)
   }
   else {
     element.className = 'not-filled'
+    spacesFilled.splice(spacesFilled.indexOf(element.id), 1)
   }
+  localStorage.setItem("Spaces Filled", JSON.stringify(spacesFilled))
 }
 
 const boardDiv = document.getElementById("board")
@@ -16,12 +20,15 @@ for (let score in scores) {
   let tableRow = document.createElement("tr")
   tableRow.innerHTML = `<th>${scores[score]}</th>`
   for (let i in catagories){
-    tableRow.innerHTML += `<td class="not-filled" value="${Number(score * catagories.length) + Number(i)}"></td>`
+    tableRow.innerHTML += `<td class="not-filled" id="${score * catagories.length + i}"></td>`
   }
   boardDiv.appendChild(tableRow)
 }
 
 const boardGrid = document.getElementsByTagName("td")
 for (let item of boardGrid){
-  item.addEventListener("click", () => changeColor(item))
+  if (spacesFilled.indexOf(item.id) != -1){
+    item.className = "filled"
+  }
+  item.addEventListener("click", () => clearSpace(item))
 }
